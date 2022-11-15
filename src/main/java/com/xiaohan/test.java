36333d6e9d938ed1668735d7f871a4057e863006
dongxiaohan <1977284986@qq.com>
2022-11-15 09:39:43 +0800

    hot-fix commit1

--- src/main/java/com/xiaohan/test.java
+++ src/main/java/com/xiaohan/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("hello dongxiaohan");
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello gitee");
     }
 }
