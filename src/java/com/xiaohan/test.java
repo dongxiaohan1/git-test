04a927b38e95cfb1c177bb5759d7f9416edea3ec
dongxiaohan <1977284986@qq.com>
2022-11-15 10:30:14 +0800

    push commit

--- src/java/com/xiaohan/test.java
+++ src/java/com/xiaohan/test.java
@@ -1,10 +1,10 @@
-package com.xiaohan;
-
+package src.java.com.xiaohan;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello dongxiaohan");
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("push");
     }
 }
